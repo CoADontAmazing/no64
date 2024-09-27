e642b73cef72d1954ce45e8bfec94bc5e70a3c29
CoAmazing <116113452+CoADontAmazing@users.noreply.github.com>
2024-09-27 20:38:47 +0300

    Add files via upload

---
+++ src/main/java/net/coamazing/no64/No64.java
@@ -0,0 +1,8 @@
+package net.coamazing.no64;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class No64 implements ClientModInitializer {
+	public static final String VERSION = "1", ID = "no64", NAME = "No64";	
+	@Override public void onInitializeClient() {}
+}
